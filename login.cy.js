bc679e4b8c75f483c5bac2715c690e6ecd261189
SZandi0022 <162721033+SZandi0022@users.noreply.github.com>
2024-03-12 07:11:18 +0330

    Add files via upload

--- login.cy.js
+++ login.cy.js
@@ -1,16 +1,18 @@
 /// <refrence types = *cypress* />
-beforeEach('test login ',function(){
 describe('test suit login',function(){
-    it.only('doshboard test', ()=>{
+    it ('doshboard test', ()=>{
         cy.visit("./project/login/login.html")
+        
+    
         cy.get('#login-email').type("saeedzandi29@gmail.com",{delay:100})
         cy.get('#login-password').type("abc123456789",{delay:100})
-      cy.get("button[type=submit]").click()
+      /cy.get("button[type=submit]").click()
     })
     })
     //test keyword 
     describe('test keyword login',function(){
         it ('doshboard test', ()=>{
+            cy.visit("./project/login/login.html")
         
             cy.get('#login-email').type("saeedzandi29@gmail.com",{delay:50}).clear().type("saeedzandi29@gamil.com ")
             cy.get('#login-password').type("abc123456789",{delay:50}).type('{backspace}{home}{selectall}{del}')
@@ -20,21 +22,23 @@ describe('test suit login',function(){
         })
         describe('test muose action',function(){
             it ('mouse action', ()=>{
+                cy.visit("./project/login/login.html")
+            
                 cy.get('#login-email').type("saeedzandi29@gmail.com",{delay:50}).dblclick().should('have.value','saeedzandi29@gmail.com')
                 cy.get('#login-password').type("abc123456789",{delay:50})
+
                 cy.contains("Log In").click()
-              cy.get('a').click()
+                cy.get('a').click()
                
             })
-            
-        })  
-        //test key hold     
-           it('test key holde', ()=>{
-            
-                 cy.get('#login-email').type("saeedzandi29@gmail.com")
-                 .realpress(['control ',' A'])
-                 .realpress(['control ',' X'])
-                .realpress(['control ','V'])
-                cy.get('#login-password').type("abc123456789").click
             })
-        })
+        
+           //* it('test key holde', ()=>{
+               //* cy.visit("./project/login/login.html")
+            
+                //* cy.get('#login-email').type("saeedzandi29@gmail.com")
+                //* .realpress(['control ',' A'])
+                //* .realpress(['control ',' X'])
+               //  .realpress(['control ','V'])
+               //* cy.get('#login-password').type("abc123456789").click
+            //* })
